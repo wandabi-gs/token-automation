6f3f0668fd9880a86877175faa654de046acd0d1
tonylloyd2 <101291544+tonylloyd2@users.noreply.github.com>
2023-03-26 12:23:06 +0300

    callback

--- transactions/callback.php
+++ transactions/callback.php
@@ -4,20 +4,19 @@ require($_SERVER["DOCUMENT_ROOT"] . "/db_connect.php");
 $data = file_get_contents('php://input');
 $decoded_data = json_decode($data);
 
-// Check if the transaction was successful
-if ($decoded_data->Body->stkCallback->ResultCode == 0) {
-    $merchant_request_id = $decoded_data->Body->stkCallback->MerchantRequestID;
-    $checkout_request_id = $decoded_data->Body->stkCallback->CheckoutRequestID;
-    $result_description = $decoded_data->Body->stkCallback->ResultDesc;
-
-    // Update the transaction status in the database
-    $stmt = $conn->prepare("UPDATE transactions SET status = 'completed', result_description = ? WHERE merchant_request_id = ?");
-    $stmt->bind_param("ss", $result_description, $merchant_request_id);
-    $stmt->execute();
-    $stmt->close();
-
-    // Perform other necessary actions, such as sending a confirmation message to the customer
-    // ...
+if ($decoded_data->Body->stkCallback->ResultCode != 0) {
+    // Transaction failed, do not register the log in the database
+    return;
 }
 
+// Transaction succeeded, register the log in the database
+$merchant_request_id = $decoded_data->Body->stkCallback->MerchantRequestID;
+$checkout_request_id = $decoded_data->Body->stkCallback->CheckoutRequestID;
+$result_description = $decoded_data->Body->stkCallback->ResultDesc;
+
+$stmt = $conn->prepare("UPDATE transactions SET status = 'completed', result_description = ? WHERE merchant_request_id =
+?");
+$stmt->bind_param("ss", $result_description, $merchant_request_id);
+$stmt->execute();
+$stmt->close();
 ?>
